ab8ed92f1f8bdb4fd64d2763c718967091096ce1
Renat Sergazinov <mrsergazinov@tamu.edu>
2020-09-27 16:40:35 -0500

    Changed code to run on CPU=based TF

--- test_functions.py
+++ test_functions.py
@@ -5,9 +5,9 @@ This code defines functions to test CNN models on images.
 """
 
 import tensorflow as tf
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
+""" physical_devices = tf.config.experimental.list_physical_devices('GPU')
 assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+tf.config.experimental.set_memory_growth(physical_devices[0], True) """
 
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Sequential, load_model
