5a1dff68c00d2346fa6b02157b6bdcfac2ab6f96
Renat Sergazinov <mrsergazinov@tamu.edu>
2021-04-22 12:46:53 -0500

    Uploaded training/testing logs; ingoring saved models/data

--- datagen_template.py
+++ datagen_template.py
@@ -16,82 +16,74 @@ if __name__ == '__main__':
     # set particle parameters
     #num forces
     num_forces = 2     
-    #number of different magnitudes for forces
+    # number of different magnitudes for forces
     num_mags = 4
-    #number of different angles 
+    # number of different angles 
     num_angles_inner = 4
     num_angles_tang = 4
-    #number of randomly noised pictures for each force selection    
+    # number of randomly noised pictures for each force selection    
     num_random = 1
     
-    #max/min magnitude for force
-    lower_bound = 0.0001
-    upper_bound = 0.4
+    # max/min magnitude for force
+    lower_bound = 0.01
+    upper_bound = 0.9
+    
+    # delta angle between contact forces
+    delta_angle_inner = np.pi / 6
 
     #radius/height pf particle
-    radius = 0.0055
-    height = 1
+    radius = [0.0055]
+    height = 0.005
     
     # material constant
-    f_sigma = 100.24
+    f_sigma = 11000
     # pixels per radius
-    pixels_per_radius = 40
+    px2m = 0.00019
     # brightness cutoff
-    cutoff = 10000
+    cutoff = np.infty
     
-# check if input is supplied from command line
-    if (len(sys.argv) == 2):
-        num_forces = int(sys.argv[1])
-    elif (len(sys.argv) == 6):
-        num_forces = int(sys.argv[1])
-        num_mags = int(sys.argv[2])
-        num_angles_inner = int(sys.argv[3])
-        num_angles_tang = int(sys.argv[4])
-        num_random = int(sys.argv[5])
-    elif (len(sys.argv) == 13):
-        num_forces = int(sys.argv[1])
-        num_mags = int(sys.argv[2])
-        num_angles_inner = int(sys.argv[3])
-        num_angles_tang = int(sys.argv[4])
-        num_random = int(sys.argv[5])
-        lower_bound = float(sys.argv[6])
-        upper_bound = float(sys.argv[7])
-        radius = float(sys.argv[8])
-        height = float(sys.argv[9])
-        f_sigma = float(sys.argv[10])
-        pixels_per_radius = int(sys.argv[11])
-        cutoff = float(sys.argv[12])
-    
+    # check input is supplied from command line
+    if (sys.argv[1] == 'user_input'):
+        radius = []
+        num_radius = int(sys.argv[2])
+        for i in range(num_radius):
+            radius.append(float(sys.argv[3 + i]))
+        num_forces = int(sys.argv[2 + num_radius + 1])
+        num_mags = int(sys.argv[2 + num_radius + 2])
+        num_angles_inner = int(sys.argv[2 + num_radius + 3])
+        num_angles_tang = int(sys.argv[2 + num_radius + 4])
+        num_random = int(sys.argv[2 + num_radius + 5])
+        subset = sys.argv[2 + num_radius + 6]
+
     #multiprocessing
     num_processes = cpu_count()
     pool = Pool(processes = num_processes)
     
-    #image generator with preset parameters
-    particle = Particle(radius, height)
-    image_gen_preset = partial(photo_elastic_response_on_particle, particle, f_sigma, pixels_per_radius, cutoff)
-    
     #generate force lists
-    list_of_F = list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_inner, num_random, lower_bound, upper_bound)
-    #save force descriptions for each particle
+    list_of_F = list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_inner, num_random, lower_bound, upper_bound, delta_angle_inner)
+    #save force descriptions
     labels_angles_inner = np.array([[f.get_phi() for f in F] for F in list_of_F])
     labels_angles_tang = np.array([[f.get_alpha() for f in F] for F in list_of_F])
     labels_mags = np.array([[f.get_mag() for f in F] for F in list_of_F])
-    #generate images of particles based on the force lists
-    data = pool.map(image_gen_preset, list_of_F)
-    
-    print(num_forces)
-    # save images
-    for i in range(len(data)):
-        plt.imsave(os.path.join(os.getcwd(), 'image_data', 'train', str(num_forces), 'img' + str(i) + '.jpg'),
-                   np.asarray(data[i]), 
-                   vmin= 0, vmax = 1, 
-                   cmap='gray')
-    np.save(os.path.join(os.getcwd(), 'labels', 'train', str(num_forces), 'angles_inner.npy'),
-            labels_angles_inner)
-    np.save(os.path.join(os.getcwd(), 'labels', 'train', str(num_forces), 'angles_tang.npy'),
+    np.save(os.path.join(os.getcwd(), 'labels', subset, str(num_forces), 'angles_inner.npy'),
+                labels_angles_inner)
+    np.save(os.path.join(os.getcwd(), 'labels', subset, str(num_forces), 'angles_tang.npy'),
             labels_angles_tang)
-    np.save(os.path.join(os.getcwd(), 'labels', 'train', str(num_forces), 'mags.npy'),
+    np.save(os.path.join(os.getcwd(), 'labels', subset, str(num_forces), 'mags.npy'),
             labels_mags)
-
+    
+    for r in radius:
+        #image generator with preset parameters
+        particle = Particle(r, height)
+        pixels_per_radius = int(round(r / px2m))
+        image_gen_preset = partial(photo_elastic_response_on_particle, particle, f_sigma, pixels_per_radius, cutoff)
         
+        #generate images of particles based on the force lists
+        data = pool.map(image_gen_preset, list_of_F)
+        
+        for i in range(len(data)):
+            plt.imsave(os.path.join(os.getcwd(), 'image_data', subset, str(num_forces), 
+                                    'r' + str(r) + '_img' + str(i) + '.jpg'), np.asarray(data[i]), vmin = 0, vmax = 1, cmap = 'gray')
+
+        print(r, num_radius, num_forces, num_mags, num_angles_inner, num_angles_tang, num_random)
     
--- force_list.py
+++ force_list.py
@@ -5,6 +5,7 @@ are going to obey the requirments postulated for the uniformly-sized particles.
 """
 
 import numpy as np
+from scipy.stats import truncexpon
 from math import sin, cos, asin, acos, pi, sqrt
 from classes import *
 
@@ -42,12 +43,18 @@ def calculate_last_force(F):
     sin_val = y_component_inner / inner_magnitude
     
     f_last.magnitude = sqrt(inner_magnitude**2 + tang_component**2)
-    f_last.phi = (angle_finder(cos_val, sin_val)+pi)%(2*pi)
-    f_last.alpha = asin(tang_component/f_last.get_mag())
+    f_last.phi = (angle_finder(cos_val, sin_val) + pi) % (2 * pi)
+    f_last.alpha = asin(tang_component / f_last.get_mag())
 
     return f_last
+
+def calculate_conv_angle(angle):
+    if angle > pi:
+        return (2 * pi - angle)
+    else:
+        return angle
         
-def list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_inner, num_random, f_lower_bound, f_upper_bound):
+def list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_inner, num_random, f_lower_bound, f_upper_bound, delta_angle_inner):
     '''
     This function generates force lists.
     
@@ -65,17 +72,16 @@ def list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_
                 0 and std.dev = pi/12, which is to ensure that the tangential anle is mostly
                 within pi/4 and -pi/4
                 c. The interval [0, 2*pi] is divided into num_forces equal sub-intervals, 
-                such that these sub-intervals are pi/3 apart. The position angle for each 
+                such that these sub-intervals are delta_angle_inner apart. The position angle for each 
                 of the forces is drawn from uniform distribition over the correspodning 
-                sub-interval. Such procedure is necessary to ensure that the forces are 
-                pi/3 apart from each other.
+                sub-interval.
             ii. The last force is calculated to ensure that the balance equations are satisfied
-            iii. The forces are checked again to ensure that they are at least pi/3 apart
-            and each has the tangential component within -pi/4 to pi/4.This is necessary as
+            iii. The forces are checked again to ensure that they are at least delta_angle_inner apart
+            and each has the tangential component within -pi/4 to pi/4. This is necessary as
             the last force may have changed this. 
             iv. If the conditions are satisfied then we have a ready-to-go force list. We
             repetitively (num_angles_inner times) add this force list to the final list of force lists as follows:
-                a. At each step, we shift all of the position angles by 2*pi/num_angles_inner.
+                a. At each step, we shift all of the position angles by 2*pi / (num_forces * num_angles_inner).
                 b. We attach num_random copies of the current force list to the final list of 
                 force lists.
     
@@ -85,31 +91,33 @@ def list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_
     '''
     list_of_F_lists = []
     
-    #delta serves to ensure that the sub-inrervals for position angles are pi/3 apart
-    #shift is defined to produce the sub-intervals for position angles from the interval [0,2*pi]
-    #epsilon is defined to shift the angles while attached the force list to the final list of 
-    #force lists
-    #phi_init is defined to help produce the sub-intervals for position angles from the interval
-    #[0, 2*pi]
-    delta = pi/6
+    # delta serves to ensure that the sub-inrervals for position angles are pi/3 apart
+    # shift is defined to produce the sub-intervals for position angles from the interval [0,2*pi]
+    # epsilon is defined to shift the angles while attached the force list to the final list of 
+    # force lists
+    # phi_init is defined to help produce the sub-intervals for position angles from the interval
+    # [0, 2*pi]
     shift = 2 * pi / num_forces
-    epsilon = 2*pi/num_angles_inner
+    epsilon = shift / num_angles_inner
     phi_init = 0  
     
-    #alpha_init and alpha_std_dev are parameters for the random normal distribution to produce
-    #tangential angles
-
+    # alpha_init and alpha_std_dev are parameters for the random normal distribution to produce
+    # tangential angles
+    delta = delta_angle_inner / 2
     alpha_init = 0
     alpha_std_dev = pi/12
-    #the std. dev was chosen so that alpha mostly stays within -pi/4 to pi/4, which is
-    #needed because |f_tang| < |f_inner| due to physical constraints
+    # the std. dev was chosen so that alpha mostly stays within -pi/4 to pi/4, which is
+    # needed because |f_tang| < |f_inner| due to physical constraints
     
-    #max_attempts is defined to limit the number of attempts to produce a force list given a
-    #starting magnitude. This is done to speed up computations. 
+    # max_attempts is defined to limit the number of attempts to produce a force list given a
+    # starting magnitude. This is done to speed up computations. 
     max_attempts = 10**6
     
     # draw mean force
-    for mag in np.linspace(f_lower_bound, f_upper_bound, num_mags):
+    scale = 1 / 2
+    rv = truncexpon(b = (f_upper_bound-f_lower_bound) / scale, loc = f_lower_bound, scale = scale)
+    mags = rv.rvs(num_mags)
+    for mag in mags:
         for i in range(num_angles_tang):
             # count attempts made to generate new force list
             attempt_count = 0
@@ -118,8 +126,8 @@ def list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_
                 for i in range(num_forces-1):
                     # generate (num_forces - 1) forces acting on particle
                     f_mag = abs(np.random.normal(mag, mag/5))
-                    f_phi = np.random.uniform(phi_init+i*shift+delta, 
-                                            phi_init+(i+1)*shift-delta) % (2*pi)
+                    f_phi = np.random.uniform(phi_init + i * shift + delta, 
+                                            phi_init + (i + 1) * shift - delta) % (2 * pi)
                     f_alpha = np.random.normal(alpha_init, alpha_std_dev)
                     F_list.append(Force(f_mag, f_phi, f_alpha))
                 # calculate last force to balance the rest
@@ -127,7 +135,7 @@ def list_of_force_angle_lists(num_forces, num_mags, num_angles_tang, num_angles_
                 F_list.append(f_last)
                 
                 # check that the generated forces are physically feasible
-                check = [abs(f_last.get_phi() - f.get_phi()) >= pi/3 for f in F_list[:-1]]
+                check = [calculate_conv_angle((f_last.get_phi() - f.get_phi()) % (2 * pi)) >= delta_angle_inner for f in F_list[:-1]]
                 check += [(f.get_alpha()<=pi/4 and f.get_alpha()>=-pi/4) for f in F_list]
                 check += [(f.get_mag() >= 0) for f in F_list]
                 attempt_count+=1
